a00579755b0cb8af23dd7e68abed96ec21a3cb31
stepanio17 <92565367+stepanio17@users.noreply.github.com>
2026-04-27 18:46:20 +0700

    fix mistakes with input of servings

--- schemas.py
+++ schemas.py
@@ -4,7 +4,7 @@ from typing import Optional
 class RecipeBase(BaseModel):
     title: str = Field(..., min_length=1, max_length=70, description="Название не может быть пустым")
     description: str = Field(..., min_length=10)
-    servings: int = Field(..., gt=0, description="Количество порций должно быть больше нуля")
+    servings: int = Field(..., gt=0, le=16, description="Количество порций должно быть больше нуля")
     category: Optional[str] = Field(None, max_length=30)
     image_url: Optional[str] = None
 
