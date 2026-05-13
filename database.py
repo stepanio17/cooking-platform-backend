3e265a52591c57ce76141ec676ca81b33fb10d3e
stepanio17 <92565367+stepanio17@users.noreply.github.com>
2026-05-13 17:00:54 +0700

    add docker connection

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:123@localhost:5432/CookingPlatform"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://user:password@db:5432/cooking_platform"
+)
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
